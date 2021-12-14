74a5efc06622c5f726b15281ed4f94dbdf18f0bf
Jeff Webb <jeff.webb@codecraftsmen.org>
2021-12-14 10:48:12 -0600

    Cleaned up option.group decorator by refactoring code into a derived class

--- test/run_test.py
+++ test/run_test.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 
 import fire
-from ticli.option import *
+from ticli import option
 
-@options
-class Options:
+@option.group
+class Test_Options:
     """
     Class.
 
     More Class.
     """
     
-    def __init__(self, x:int=8, y:int=9):
-        """ Args:
-              x: first init arg
-              y: second init arg
-        """
-        print(f"_init: x:{x} y:{y}")
-
-    def _check_options(self, a:int=1, b:int=2):
+    def _options(self, a:int=1, b:int=2):
         """ Args:
               a: first arg
               b: second arg
         """
         print(f"_check_options: a:{a} b:{b}")
+
+    def _init(self, x:int=8, y:int=9):
+        """ Args:
+              x: first init arg
+              y: second init arg
+        """
+        print(f"_init: x:{x} y:{y}")
 
     def _invoke(self, invoke1):
         """ Args:
@@ -33,8 +33,9 @@ class Options:
         return self
     
 if __name__ == '__main__':
+    
     # Work-around to keep fire from using "less" to show help output
     import os
     os.environ["PAGER"] = "cat"
     
-    fire.Fire(Options)
+    fire.Fire(Test_Options)
--- ticli/option.py
+++ ticli/option.py
@@ -3,98 +3,112 @@ from makefun import with_signature
 from decopatch import class_decorator, DECORATED
 
 @class_decorator
-def options(C=DECORATED):
+def group(C=DECORATED):
 
-    # Provide default implementations for required classes, if necessary
-    if not "_check_options" in C.__dict__:
-        def _check_options(self):
-            pass
-        C._check_options = _check_options
-        
-    if not "__init__" in C.__dict__:
-        def __init__(self):
-            pass
-        C.__init__ = __init__
-        
-    if not "_invoke" in C.__dict__:
-        def _invoke(self):
-            pass
-        C._invoke = _invoke
-
-    # This is used in __init__()
-    C._option_names = inspect.getfullargspec(C._check_options)[0][1:]
-
-    # Extract the user-defined method signatures
-    options_sig = inspect.signature(C._check_options)
-    option_params = list(options_sig.parameters.values())[1:]
-    init_sig = inspect.signature(C.__init__)
-    init_params = list(init_sig.parameters.values())
-    invoke_sig = inspect.signature(C._invoke)
-    invoke_params = list(invoke_sig.parameters.values())
+    # Extract the option parameters
+    def _extract_option_params(_options):
+        options_sig = inspect.signature(_options)
+        option_params = list(options_sig.parameters.values())[1:]
+        return option_params
 
     # Construct an __init__() signature that includes options
-    leading_init_params = list(init_params)
-    trailing_init_params = []
-    for p in reversed(init_params):
-        if p.kind in [ inspect.Parameter.VAR_KEYWORD,
-                       inspect.Parameter.KEYWORD_ONLY,
-                       inspect.Parameter.VAR_POSITIONAL]:
-            trailing_init_params.insert(0, leading_init_params.pop())
-    new_init_sig = init_sig.replace(
-        parameters=leading_init_params + option_params + trailing_init_params)
-
-    # Define an __init__() that handles options
-    @with_signature(new_init_sig)
-    def __init__(self, *args, **kw):
-        option_kw = {}
-        for o_name in self._option_names:
-            if o_name in kw:
-                option_kw[o_name] = kw.pop(o_name)
-        self._check_options(**option_kw)
-        return self._init(*args, **kw)
-    C._init = C.__init__
-    C.__init__ = __init__
-
+    def _construct_init_sig(_init, option_params):
+        init_sig = inspect.signature(_init)
+        init_params = list(init_sig.parameters.values())
+        leading_init_params = list(init_params)
+        trailing_init_params = []
+        for p in reversed(init_params):
+            if p.kind in [ inspect.Parameter.VAR_KEYWORD,
+                           inspect.Parameter.KEYWORD_ONLY,
+                           inspect.Parameter.VAR_POSITIONAL ]:
+                trailing_init_params.insert(0, leading_init_params.pop())
+        new_init_sig = init_sig.replace(
+            parameters = leading_init_params + option_params +
+                         trailing_init_params)
+        return new_init_sig
+    
     # Construct a __call__() signature that includes options
-    leading_invoke_params = list(invoke_params)
-    trailing_invoke_params = []
-    for p in reversed(invoke_params):
-        if p.kind in [ inspect.Parameter.VAR_KEYWORD,
-                       inspect.Parameter.KEYWORD_ONLY,
-                       inspect.Parameter.VAR_POSITIONAL]:
-            trailing_invoke_params.insert(0, leading_invoke_params.pop())
-    call_sig = invoke_sig.replace(
-        parameters=leading_invoke_params + option_params +\
-                   trailing_invoke_params)
-    call_params = list(call_sig.parameters.values())
-        
-    # Define a __call__() that handles options before calling _invoke()
-    @with_signature(call_sig)
-    def __call__(self, *args, **kw):
-        option_kw = {}
-        for o_name in self._option_names:
-            if o_name in kw:
-                option_kw[o_name] = kw.pop(o_name)
-        self._check_options(**option_kw)
-        return self._invoke(*args, **kw)
-    C.__call__ = __call__
-
+    def _construct_call_sig(_invoke, option_params):
+        invoke_sig = inspect.signature(_invoke)
+        invoke_params = list(invoke_sig.parameters.values())
+        leading_invoke_params = list(invoke_params)
+        trailing_invoke_params = []
+        for p in reversed(invoke_params):
+            if p.kind in [ inspect.Parameter.VAR_KEYWORD,
+                           inspect.Parameter.KEYWORD_ONLY,
+                           inspect.Parameter.VAR_POSITIONAL]:
+                trailing_invoke_params.insert(0, leading_invoke_params.pop())
+        call_sig = invoke_sig.replace(
+            parameters=leading_invoke_params + option_params + \
+                       trailing_invoke_params)
+        return call_sig
+    
     # Piece together top-level documentation string
-    C._doc_sources = [
-        C,
-        C._init,
-        C._invoke,
-        C._check_options,
-    ]
-    for o in C._doc_sources:
-        setattr(o, "_user_doc", getattr(o, "__doc__"))
-    @classmethod
-    def _make_doc(C):
-        docs = []
-        for o in C._doc_sources:
-            docs.append("" if o._user_doc is None else o._user_doc)
-        C.__doc__ = "".join(docs)
-    C._make_doc = _make_doc
-    C._make_doc()
+    def _make_doc(cls):
+        methods_with_doc = [
+            "_options",
+            "_init",
+            "_invoke",
+        ]
+        docs = [cls.__doc__]
+        for m_name in methods_with_doc:
+            doc = ""
+            if hasattr(cls, m_name):
+                m = getattr(cls, m_name)
+                if hasattr(m, "__doc__"):
+                    doc = m.__doc__
+            docs.append(doc)
+        return "".join(docs)
+        
+    class D(C):
+        # Provide default implementations for required methods, if necessary
+        if hasattr(C, "_options"):
+            _options = C._options
+        else:
+            print("adding _options")
+            def _options(self):
+                pass
+        if hasattr(C, "_init"):
+            _init = C._init
+        else:
+            print("adding _init")
+            def _init(self):
+                pass
+        if hasattr(C, "_invoke"):
+            _invoke = C._invoke
+        else:
+            print("adding _invoke")
+            def _invoke(self):
+                pass
 
-    return C
+        # Extract information about options from the method signature
+        _option_params = _extract_option_params(_options)
+        _option_names = inspect.getfullargspec(_options)[0][1:]
+        
+        # Define an __init__() that handles options before calling _init()
+        _init_sig = _construct_init_sig(_init, _option_params)
+        @with_signature(_init_sig)
+        def __init__(self, *args, **kw):
+            option_kw = {}
+            for o_name in self._option_names:
+                if o_name in kw:
+                    option_kw[o_name] = kw.pop(o_name)
+            self._options(**option_kw)
+            return self._init(*args, **kw)
+        
+
+        # Define a __call__() that handles options before calling _invoke()
+        _call_sig = _construct_call_sig(_invoke, _option_params)    
+        @with_signature(_call_sig)
+        def __call__(self, *args, **kw):
+            option_kw = {}
+            for o_name in self._option_names:
+                if o_name in kw:
+                    option_kw[o_name] = kw.pop(o_name)
+            self._options(**option_kw)
+            return self._invoke(*args, **kw)
+    
+        # Piece together top-level documentation string
+        __doc__ = _make_doc(C)
+    
+    return D
