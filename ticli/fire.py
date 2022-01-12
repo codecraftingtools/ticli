d387d6d13ad393efb762495119d34779ad6016de
Jeff Webb <jeff.webb@codecraftsmen.org>
2022-01-11 20:44:01 -0600

    Added automatic argument validation when using Fire on functions

--- ticli/fire.py
+++ ticli/fire.py
@@ -4,6 +4,9 @@
 Wrapper for the Python Fire package.
 """
 
+import inspect
+from .validation import validate_arguments
+
 try:
     from fire import decorators
     from fire import Fire
@@ -23,14 +26,18 @@ except:
     decorators.FIRE_STAND_IN = "FIRE_STAND_IN"
 
 _Fire = Fire
-def Fire(*args, **kw):
+def Fire(component, *args, **kw):
     # Work-around to keep fire from using "less" to show help output
     import os
     os.environ["PAGER"] = "cat"
-    
+
+    # Automatically add validation for functions
+    if inspect.isfunction(component):
+        component = validate_arguments(component)
+        
     # Provide exception handling for validation errors
     import ticli.validation as v
     try:
-        _Fire(*args, **kw)
+        _Fire(component, *args, **kw)
     except v.ValidationError as exc:
         v.print_validation_error(exc)
